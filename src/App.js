285fa82d35bfbaefa57ddb592c2012b0023ca0bf
mansur2021 <102415890+mansur2021@users.noreply.github.com>
2022-06-07 15:02:19 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./layout/Layout";
+
+
+import Albums from "./containers/Albums/Albums";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+     <Route path="albums" element={<Albums />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;

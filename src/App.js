54d5653d98bf14d86c33006d9b58981b9e54a3af
mansur2021 <102415890+mansur2021@users.noreply.github.com>
2022-06-07 15:36:04 +0300

    commit 5

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 
-
 import Albums from "./containers/Albums/Albums";
 
 function App() {
@@ -11,7 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-     <Route path="albums" element={<Albums />} />
+          <Route path="albums" element={<Albums />} />
         </Route>
       </Routes>
     </>
